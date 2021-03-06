f25eea989988357698de9a1503d2d3af1dc91d01
guivargas95 <77880134+guivargas95@users.noreply.github.com>
2021-03-06 17:35:39 -0300

    Update ex071.py

--- ex071.py
+++ ex071.py
@@ -1,28 +1,16 @@
-res = cont = int(0)
-val = float(input('Qual o valor do saque? R$ '))
-nota = int(50)
+notas = [1,2,5,10,20,50,100,200]
+cont = int(0)
+saque = int(input('Informe o valor do saque: R$ '))
 while True:
-    if val >= nota:
-        val = val - nota
-        cont += 1
+    if saque <= 600 and saque >= 10:
+        break
     else:
-        if cont > 0:
-            print(f'Total de {cont} cédulas de {nota}')
-        if nota == 50:
-            nota = 20
-            cont = 0
-        elif nota == 20:
-            nota = 10
-            cont = 0
-        elif nota == 10:
-            nota = 5
-            cont = 0
-        elif nota == 5:
-            nota = 2
-            cont = 0
-        elif nota ==2:
-            nota = 1
-            cont = 0
-        elif nota == 1:
-            break
+        saque = int(input('Valor inválido! Informe um valor entre R$10 e R$ 600. Valor do saque: R$ '))
+for c in reversed(notas):
+    if c <= saque:
+        while c <= saque:
+            saque = saque - c
+            cont = cont + 1
+        print(f'Total de {cont} notas de R${c}')
+        cont = 0
 
